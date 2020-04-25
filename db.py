06225ced0fc81942519959a692cb68d0e532f11b
mathew pius <mathewpius4232@gmail.com>
2020-04-25 18:36:40 +0530

    Final HTML and CSS Files

--- db.py
+++ db.py
@@ -120,6 +120,7 @@ def add_server_post():
             response_message['message']="IP Address cannot be NULL or empty"
         else:
             conn.execute(f'insert into {username}_servers (IP_Address,Server_Name) values {ip_address,server_name}')
+            conn.execute(f"create table if not exists {username}_{server_name} (HEALTH_ID integer primary key AUTOINCREMENT,Time_Epoch integer,Disk_Free varchar(80),Bytes_Sent varchar(80),Bytes_Received varchar(80),Packets_Sent varchar(80),Packets_Received varchar(80),Memory_Free varchar(80),Cpu_Usage_Percent varchar(80),Cpu_Time varchar(80));")
             response_message['message']="Server Registration Successful"
     finally:
         conn.commit()
@@ -163,37 +164,16 @@ def user():
     username=object['Username']
     conn=sqlite3.connect("Health.db")
     cur=conn.cursor()
-    response_message={"message":"Adding Server...."}
-    try:
-        cur.execute(f"select * from {username}_servers order by IP_ID")
-        user_dict={"ID":[],"IP_Address":[],"Server_Name":[]}
-        for row in cur:
-            user_dict['ID'].append(row[0])
-            user_dict['IP_Address'].append(row[1])
-            user_dict['Server_Name'].append(row[2])
-            if len(user_dict['id'])>5:
-                raise Exception
-    except Exception:
-        response_message['message']="No of Servers are full.Delete a Server to continue!"
-    else:
-        user=json.dumps(user_dict)
-        
-    finally:
+    cur.execute(f"select * from {username}_servers order by IP_ID")
+    user_dict={"ID":[],"IP_Address":[],"Server_Name":[]}
+    for row in cur:
+        user_dict['ID'].append(row[0])
+        user_dict['IP_Address'].append(row[1])
+        user_dict['Server_Name'].append(row[2])
         conn.commit()
-        return user
-
+    user=json.dumps(user_dict)
+    return user
     
-
-@app.route("/Delete_User",methods=['POST'])
-def del_user():
-    object=request.json
-    username=object['Username']
-    servername=object['Server Name']
-    conn=sqlite3.connect("Health.db")
-    conn.execute(f'drop table {username}_{servername}')
-    conn.execute(f'drop table {username}_servers')
-    conn.execute()
-    return "Deletion Successful"
 @app.route("/Add_Server",methods=['GET'])
 def add_server_get():
     return render_template("Server.html")
@@ -202,7 +182,6 @@ def add_server_get():
 def report():
     time_epoch=time.time()
     incoming_report = request.get_json()
-    print("Generating Health report")
     username=incoming_report["USER_NAME"]
     server_name=incoming_report["SERVER_NAME"]
     disk_free=incoming_report["free_Percnt"]
@@ -214,44 +193,104 @@ def report():
     cpu_percent=incoming_report["cpupercent"]
     cpu_total=incoming_report["cpu_total"]
     conn=sqlite3.connect("Health.db")
-    conn.execute(f"create table if not exists {username}_{server_name} (HEALTH_ID integer primary key AUTOINCREMENT,Time_Epoch integer,Disk_Free varchar(80),Bytes_Sent varchar(80),Bytes_Received varchar(80),Packets_Sent varchar(80),Packets_Received varchar(80),Memory_Free varchar(80),Cpu_Usage_Percent varchar(80),Cpu_Time varchar(80));")
-    conn.execute(f'insert into {username}_{server_name} (Time_Epoch,Disk_Free,Bytes_Sent,Bytes_Received,Packets_Sent,Packets_Received,Memory_Free,Cpu_Usage_Percent,Cpu_Time) values {time_epoch,disk_free,bytes_sent,bytes_received,packets_sent,packets_received,memory_free,cpu_percent,cpu_total}')
-    conn.commit()
-    return "yes"  
+    response_message={"message":"Generating Health Report"}
+    try:
+        cur=conn.cursor()
+        cur.execute("select exists(select username from user where username=(?))",(username,))
+        for row in cur:
+            rowlist=list(row)
+            if username=="NULL" or not username or username.isspace() or rowlist[0]==0:
+                    raise Exception 
+    except Exception:
+        response_message['message']="username cannot be NULL or user doesnt exist"
+        return response_message
+    else:
+        try:
+            cur=conn.cursor()
+            cur.execute(f"select exists(select Server_Name from {username}_servers where Server_Name=(?))",(server_name,))
+            for row in cur:
+                rowlist=list(row)
+                if server_name=="NULL" or not server_name or server_name.isspace() or rowlist[0]==0:
+                    raise Exception 
+        except Exception:
+            response_message['message']="servername cannot be NULL or servername doesnt exist"
+            return response_message
+        else:
+            conn.execute(f'insert into {username}_{server_name} (Time_Epoch,Disk_Free,Bytes_Sent,Bytes_Received,Packets_Sent,Packets_Received,Memory_Free,Cpu_Usage_Percent,Cpu_Time) values {time_epoch,disk_free,bytes_sent,bytes_received,packets_sent,packets_received,memory_free,cpu_percent,cpu_total}')
+            response_message['message']="Generating Health Report"
+            return response_message
+    finally:
+        conn.commit()  
 
-@app.route("/Display/<servername>/<details>",methods=['GET'])
-def display(servername,details):
-    conn=sqlite3.connect("Health.db")    
+
+@app.route("/Display",methods=['POST'])
+def display():
+    object=request.json
+    username=object["Username"]
+    servername=object["Servername"]
+    details=object["Details"]
+    conn=sqlite3.connect("Health.db")
+    health_dict={'message':'Generating Health Report','Health_id':[],'Epoch_Time':[],'Disk_Free':[],'Bytes_Sent':[],'Bytes_Received':[],'Packets_Sent':[],'Packets_Received':[],'Memory_Free':[],'CPU_Usage_Percent':[],'CPU_Time':[]} 
     cur=conn.cursor()  
     try:
-        if details=="NULL" or  not details or details.isspace() or not (details=='all' or details=='last 5' or details== 'first 5'):
-            raise Exception
-    except Exception:
-        print("Invalid input")
-    else:
-        if details=='all':
-            cur.execute(f" select * from {servername} order by HEALTH_ID")
-        elif (details=='last 5'):
-            cur.execute(f" select * from {servername} order by HEALTH_ID desc limit 5")
-        else:
-            cur.execute(f" select * from {servername} order by HEALTH_ID asc limit 5")
-
-
-        health_dict={'Health_id':[],'Epoch_Time':[],'Disk_Free':[],'Bytes_Sent':[],'Bytes_Received':[],'Packets_Sent':[],'Packets_Received':[],'Memory_Free':[],'CPU_Usage_Percent':[],'CPU_Time':[]}
+        cur.execute(f"select exists(select Server_Name from {username}_servers where Server_Name=(?))",(servername,))
         for row in cur:
-            health_dict['Health_id'].append(row[0])
-            health_dict['Epoch_Time'].append(row[1])
-            health_dict['Disk_Free'].append(row[2])
-            health_dict['Bytes_Sent'].append(row[3])
-            health_dict['Bytes_Received'].append(row[4])
-            health_dict['Packets_Sent'].append(row[5])
-            health_dict['Packets_Received'].append(row[6])
-            health_dict['Memory_Free'].append(row[7])
-            health_dict['CPU_Usage_Percent'].append(row[8])
-            health_dict['CPU_Time'].append(row[9])
-        return (health_dict)
-    finally:
-        conn.commit()
+            rowlist=list(row)
+            if servername=="NULL" or not servername or servername.isspace() or rowlist[0]==0:
+                raise Exception 
+    except Exception:
+        health_dict['message']="Server Name is NULL or does not Exist"
+    else:
+        try:
+            if details=="NULL" or  not details or details.isspace() or not (details=='all' or details=='last 10' or details== 'first 10'):
+                raise Exception
+        except Exception:
+            health_dict['message']="Invalid details input."
+        else:
+            health_dict['message']="Generating Health Report"
+            cur=conn.cursor()
+            if details=='all':
+                cur.execute(f" select * from {username}_{servername} order by HEALTH_ID")
+            elif (details=='last 10'):
+                cur.execute(f" select * from {username}_{servername} order by HEALTH_ID desc limit 10")
+            else:
+                cur.execute(f" select * from {username}_{servername} order by HEALTH_ID asc limit 10")
+            
+            for row in cur:
+                health_dict['Health_id'].append(row[0])
+                health_dict['Epoch_Time'].append(row[1])
+                health_dict['Disk_Free'].append(row[2])
+                health_dict['Bytes_Sent'].append(row[3])
+                health_dict['Bytes_Received'].append(row[4])
+                health_dict['Packets_Sent'].append(row[5])
+                health_dict['Packets_Received'].append(row[6])
+                health_dict['Memory_Free'].append(row[7])
+                health_dict['CPU_Usage_Percent'].append(row[8])
+                health_dict['CPU_Time'].append(row[9])        
+        finally:
+            conn.commit()
+    health=json.dumps(health_dict)
+    return health
+#@app.route("/gen_report",methods=['POST'])
+#def gen_report():
+ #   object=request.json
+  #  username=object["Username"]
+   # servername=object["Servername"]
+    #details=object["Details"]
+    #conn=sqlite3.connect("Health.db")
+   
+    #health=json.dumps(health_dict)
+    #return health
+
+
+@app.route("/Display",methods=['GET'])
+def display_get():
+    return render_template("Display.html")
+
+@app.route("/report",methods=['GET'])
+def report_get():
+    return render_template("report.html")
+
 
 
 

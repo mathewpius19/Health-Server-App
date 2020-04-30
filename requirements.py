b378deebf3a19d71e921a86356beff33661185e6
mathew pius <mathewpius4232@gmail.com>
2020-04-30 14:52:13 +0530

    Final Updates

--- requirements.py
+++ requirements.py
@@ -2,10 +2,10 @@ import sys
 import os
 os.system("sudo apt install python3-pip")
 os.system("pip3 install psutil")
-os.system("pip3 install Flask")
+os.system("pip3 install requests")
 servername=input("Enter server name \n")
 username=input("Enter user name \n")
-os.system(f"python3 report.py {servername} {username} ")
+os.system(f"python report.py {servername} {username} ")
 
 
 

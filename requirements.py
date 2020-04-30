14c0ff19516485e7bd9d5e4b9a115ff2b0846db5
Mathew <57001761+mathewpius19@users.noreply.github.com>
2020-04-30 17:47:16 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -5,6 +5,7 @@ os.system("pip3 install psutil")
 os.system("pip3 install requests")
 servername=input("Enter server name \n")
 username=input("Enter user name \n")
+#Enter the exact same username and servername that you have registered on website.
 os.system(f"python3 report.py {servername} {username} ")
 
 

4dcb6e81d39a7dc7b74d241ee3249bab53358219
mathew pius <mathewpius4232@gmail.com>
2020-05-10 02:53:43 +0530

    Final Update

--- db.py
+++ db.py
@@ -19,24 +19,28 @@ def Regist_post():
     conn=sqlite3.connect("Health.db")
     conn.execute("create table if not exists 'user'(UID integer primary key AUTOINCREMENT,username varchar(20),password varchar(20),security varchar(20));")
     response_message={"message":"Registration Successful"}
-    count=0
+    
     try:
+        
         if len(username)<3 or not(len(security)==5):
             raise Exception
+        
         for i in username:
             if not(ord(i)>=65 and ord(i)<=90 or ord(i)>=97 and ord(i)<=122 or ord(i)>=48 and ord(i)<=57 or i=="_"):
                 raise Exception
-        for i in username:
-            if i.isdigit():
-                count+=1
-            if count == len(username):
-                raise Exception
+        
+        if username[0].isdigit():
+            raise Exception
+
         for i in security:
             if not(ord(i)>=48 and ord(i)<=57):
                 raise Exception
+    
     except Exception:
         response_message['message']="invalid username or security detail"
+    
     else:
+        
         try:
             cur=conn.cursor()
             cur.execute("select exists(select username from user where username=(?))",(username,))
@@ -44,19 +48,26 @@ def Regist_post():
                 rowlist=list(row)
                 if password=="NULL" or not password or password.isspace() or rowlist[0]==1:
                     raise Exception 
+        
         except Exception:
             response_message['message']="Password cannot be NULL or user already exists" 
+        
         else:       
             try:
                 conn.execute("insert into user(username,password,security) values(?,?,?)",(username,password,security))
+            
             except:    
                 response_message['message']="Registration Failed"
+    
     finally:
+        
         try:
             conn.commit()
+        
         except:
             response_message['message']="Registration Failed"
     response_message=json.dumps(response_message)
+    
     return response_message
 
 @app.route("/Registration",methods=['GET'])
@@ -75,20 +86,27 @@ def login_post():
     response_message={"message":"Login Successful"}
     for row in cur:
         rowlist=list(row)
+        
         try:
+            
             if rowlist[0]==0:
                 raise Exception
+        
         except Exception:
             response_message['message']="Username does not exist,Please create User and Continue"
+        
         else:
             cur.execute("select password from user where username=(?)",(username,))
             for row in cur:
                 rowlist=list(row)
+            
             try:
                 if not(rowlist[0]==password) or password=="NULL" or not password or password.isspace():
                     raise Exception
+            
             except Exception:
                 response_message['message']="Password is wrong or NULL"
+            
             else:   
                 conn.execute(f"create table if not exists {username}_servers (IP_ID integer primary key AUTOINCREMENT,IP_Address varchar(20),Server_Name varchar(80));")
                 response_message['message']="Login Successful"
@@ -119,19 +137,24 @@ def add_server_post():
             rowlist=list(row)
             if server_name=="NULL" or  not server_name or server_name.isspace() or rowlist[0]==1:
                 raise Exception
+    
     except Exception:
         response_message['message']="Server name is NULL or already exists"
             
     else:
+        
         try:
             if ip_address=='NULL' or not ip_address or ip_address.isspace():
                 raise Exception
+        
         except Exception:
             response_message['message']="IP Address cannot be NULL or empty"
+        
         else:
             conn.execute(f'insert into {username}_servers (IP_Address,Server_Name) values {ip_address,server_name}')
             conn.execute(f"create table if not exists {username}_{server_name} (HEALTH_ID integer primary key AUTOINCREMENT,Time_Epoch integer,Disk_Free varchar(80),Bytes_Sent varchar(80),Bytes_Received varchar(80),Packets_Sent varchar(80),Packets_Received varchar(80),Memory_Free varchar(80),Cpu_Usage_Percent varchar(80),Cpu_Time varchar(80));")
             response_message['message']="Server Registration Successful"
+    
     finally:
         conn.commit()
     response_message=json.dumps(response_message)
@@ -145,18 +168,22 @@ def remove_server():
     response_message={"message":"Server Registration Successful"}
     conn=sqlite3.connect("Health.db")
     cur=conn.cursor()
+    
     try:
         cur.execute(f"select exists(select Server_Name from {username}_servers where Server_Name=(?))",(servername,))
         for row in cur:
             rowlist=list(row)
             if servername=="NULL" or  not servername or servername.isspace() or rowlist[0]==0:
                 raise Exception
+    
     except Exception:
         response_message['message']="Server name is NULL or doesnt exist"
+    
     else:
         conn.execute(f'drop table {username}_{servername}')
         conn.execute(f'delete from {username}_servers where server_name=(?)',(servername,))
         response_message['message']="Server Deletion Successful"
+    
     finally:    
         conn.commit()
     responseMessage=json.dumps(response_message)
@@ -206,6 +233,7 @@ def report():
     cpu_total=incoming_report["cpu_total"]
     conn=sqlite3.connect("Health.db")
     response_message={"message":"Generating Health Report"}
+    
     try:
         cur=conn.cursor()
         cur.execute("select exists(select username from user where username=(?))",(username,))
@@ -213,10 +241,13 @@ def report():
             rowlist=list(row)
             if username=="NULL" or not username or username.isspace() or rowlist[0]==0:
                     raise Exception 
+    
     except Exception:
         response_message['message']="username cannot be NULL or user doesnt exist"
         return response_message
+    
     else:
+        
         try:
             cur=conn.cursor()
             cur.execute(f"select exists(select Server_Name from {username}_servers where Server_Name=(?))",(server_name,))
@@ -224,13 +255,16 @@ def report():
                 rowlist=list(row)
                 if server_name=="NULL" or not server_name or server_name.isspace() or rowlist[0]==0:
                     raise Exception 
+        
         except Exception:
             response_message['message']="servername cannot be NULL or servername doesnt exist"
             return response_message
+        
         else:
             conn.execute(f'insert into {username}_{server_name} (Time_Epoch,Disk_Free,Bytes_Sent,Bytes_Received,Packets_Sent,Packets_Received,Memory_Free,Cpu_Usage_Percent,Cpu_Time) values {time_epoch,disk_free,bytes_sent,bytes_received,packets_sent,packets_received,memory_free,cpu_percent,cpu_total}')
             response_message['message']="Generating Health Report"
             return response_message
+    
     finally:
         conn.commit()  
 
@@ -244,20 +278,26 @@ def display():
     conn=sqlite3.connect("Health.db")
     health_dict={'message':'Generating Health Report','Health_id':[],'Epoch_Time':[],'Disk_Free':[],'Bytes_Sent':[],'Bytes_Received':[],'Packets_Sent':[],'Packets_Received':[],'Memory_Free':[],'CPU_Usage_Percent':[],'CPU_Time':[]} 
     cur=conn.cursor()  
+    
     try:
         cur.execute(f"select exists(select Server_Name from {username}_servers where Server_Name=(?))",(servername,))
         for row in cur:
             rowlist=list(row)
             if servername=="NULL" or not servername or servername.isspace() or rowlist[0]==0:
                 raise Exception 
+    
     except Exception:
         health_dict['message']="Server Name is NULL or does not Exist"
+    
     else:
+        
         try:
             if details=="NULL" or  not details or details.isspace() or not (details=='all' or details=='last 10' or details== 'first 10'):
                 raise Exception
+        
         except Exception:
             health_dict['message']="Invalid details input."
+        
         else:
             health_dict['message']="Generating Health Report"
             cur=conn.cursor()
@@ -296,14 +336,17 @@ def security():
     conn=sqlite3.connect("Health.db")
     response_message={'message':"Answer is correct"}
     cur=conn.cursor()
+    
     try:
         cur.execute("select exists(select username from 'user' where username=(?))",(username,))
         for row in cur:
             rowlist=list(row)
             if rowlist[0]==0 or  username=="NULL" or not username or username.isspace():
                 raise Exception
+    
     except Exception:
         response_message['message']="Username does not exist"
+    
     else:
         try:
             cur.execute("select exists(select security from 'user' where security=(?))",(security,))
@@ -311,15 +354,17 @@ def security():
                 rowlist=list(row)
                 if rowlist[0]==0 or  security=="NULL" or not security or security.isspace():
                     raise Exception
+        
         except Exception:
             response_message['message']="Answer is wrong"
+        
         else:
             cur.execute("select password from user where username=(?)",(username,))
             for row in cur:
                 rowlist=list(row)
                 response_message['message']="Answer is correct"
                 response_message['password']=rowlist[0]
-            
+    
     finally:
         conn.commit()
     response=json.dumps(response_message)

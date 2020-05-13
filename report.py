1a5cf8460e22c91b1c702c015a0f476993a09319
Mathew <57001761+mathewpius19@users.noreply.github.com>
2020-05-14 01:18:55 +0530

    Update report.py

--- report.py
+++ report.py
@@ -6,42 +6,40 @@ import sys
 STATS_URL = ' http://3.134.78.37:80/report'
 
 try:
-    if len(sys.argv)<1:
-        raise Exception
-except Exception:
-        print("Invalid Entry")
+    SERVER_NAME=sys.argv[1]
+    USER_NAME=sys.argv[2]
+    print(SERVER_NAME,USER_NAME)
+except:
+    print("Invalid Entry")
+    sys.exit()
+       
 
-else:   
-        SERVER_NAME=sys.argv[1]
-        USER_NAME=sys.argv[2]
-        print(SERVER_NAME,USER_NAME)
+def get_health(): 
+    
+    print('generating health report')
 
-        def get_health(): 
+    cpu_percent = psutil.cpu_percent(interval=2.0)
+    ctime = psutil.cpu_times()
+    disk_usage = psutil.disk_usage("/")
+    net_io_counters = psutil.net_io_counters()
+    virtual_memory = psutil.virtual_memory()    
 
-            print('generating health report')
+    # The keys in this dict should match the db cols
+    report = dict (
+        USER_NAME=USER_NAME,
+        SERVER_NAME=SERVER_NAME,
+        cpupercent = cpu_percent,
+        cpu_total = ctime.user + ctime.system,
+        free_Percnt=(disk_usage.percent),
+        bytes_sent = net_io_counters.bytes_sent,
+        bytes_received = net_io_counters.bytes_recv,
+        packets_sent = net_io_counters.packets_sent,
+        packets_received = net_io_counters.packets_recv,
 
-            cpu_percent = psutil.cpu_percent(interval=2.0)
-            ctime = psutil.cpu_times()
-            disk_usage = psutil.disk_usage("/")
-            net_io_counters = psutil.net_io_counters()
-            virtual_memory = psutil.virtual_memory()    
+        memory_Free = virtual_memory.free,
+        )
 
-            # The keys in this dict should match the db cols
-            report = dict (
-                USER_NAME=USER_NAME,
-                SERVER_NAME=SERVER_NAME,
-                cpupercent = cpu_percent,
-                cpu_total = ctime.user + ctime.system,
-                free_Percnt=(disk_usage.percent),
-                bytes_sent = net_io_counters.bytes_sent,
-                bytes_received = net_io_counters.bytes_recv,
-                packets_sent = net_io_counters.packets_sent,
-                packets_received = net_io_counters.packets_recv,
-
-                memory_Free = virtual_memory.free,
-                )
-
-            return report
+    return report
 if __name__=='__main__':
 
 

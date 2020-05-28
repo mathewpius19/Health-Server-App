ad379ecc079362ff542f20d86637b891b6634989
Mathew <57001761+mathewpius19@users.noreply.github.com>
2020-05-28 19:10:56 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,11 +1,11 @@
 import sys
 import os
 os.system("sudo apt install python3-pip")
-os.system("sudo pip3 install psutil")
-os.system("sudo pip3 install requests")
+os.system("pip3 install psutil")
+os.system("pip3 install requests")
 servername=input("Enter server name \n")
 username=input("Enter user name \n")
-os.system(f"sudo python3 report.py {servername} {username} ")
+os.system(f"python3 report.py {servername} {username} ")
 
 
 

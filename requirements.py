eddf62b6d58dcae15e4f314690001264b4a4a839
Mathew <57001761+mathewpius19@users.noreply.github.com>
2020-06-18 22:28:11 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,6 +1,6 @@
 import sys
 import os
-os.system("sudo apt install python3-pip")
+os.system("apt-get install python3-pip")
 os.system("pip3 install psutil")
 os.system("pip3 install requests")
 servername=input("Enter server name \n")
